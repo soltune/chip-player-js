3090ebe19d54bbcaa15ef37649434e7687bfa6f6
soltune <k@psycube.com>
2020-08-25 11:44:59 +0900

    made simple the signature of he_set_channel_mask() in psf

--- webpsx/emscripten/adapter.cpp
+++ webpsx/emscripten/adapter.cpp
@@ -90,7 +90,7 @@ static char psfby_str[TEXT_MAX];
 	}
 
 	int he_get_psf_version (DB_fileinfo_t *_info);
-	void he_set_channel_mask (DB_fileinfo_t *_info, uint64 mask);
+	void he_set_channel_mask (DB_fileinfo_t *_info, uint32 mask0, uint32 mask1);
 	void he_set_reverb(DB_fileinfo_t *_info, uint8 enable);
 //}
 
@@ -218,7 +218,7 @@ extern "C" int EMSCRIPTEN_KEEPALIVE psf_get_psf_version() {
 
 extern "C" void psf_set_mask(uint32 mask0, uint32 mask1) __attribute__((noinline));
 extern "C" void EMSCRIPTEN_KEEPALIVE psf_set_mask(uint32 mask0, uint32 mask1) {
-	he_set_channel_mask(song_info, (mask0 + ((uint64) mask1 << 24))); // both masks are 24bits
+	he_set_channel_mask(song_info, mask0, mask1);
 }
 
 extern "C" void psf_set_reverb(uint8 enable) __attribute__((noinline));
